6d1b224558079f6b1bf482fb765765a0dd609eec
navigill7 <nnavigill784@gmail.com>
2024-04-02 13:17:53 +0530

    adding login page components

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,33 @@
+import { Box , Typography , useTheme , useMediaQuery } from "@mui/material"; 
+import  {Form} from "./Form"
 const LoginPage = ()=>{
-    return ( 
-        <div>loginpage</div>
-    )
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
+            <Typography 
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary" 
+            >
+                Uni-Link
+            </Typography>
+        </Box>
+        <Box 
+           width={isNonMobileScreens ? "50%" : "90%"}
+           p="2rem"
+           m="2rem auto"
+           borderRadius="1.5rem"
+           backgroundColor={theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{  mb: "1.5rem"}}>
+            UniLink: Connecting Minds, Building Futures
+            </Typography>
+            <Form /> 
+
+        </Box>
+
+    </Box>
 }
 
 export default LoginPage;
